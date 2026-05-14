177ba218c128d8e13532c95ed41e4ee1f2c70f47
xiaoxin1399 <xiaoxin1399@163.com>
2026-05-14 10:04:51 +0800

    C2/04

--- code/C2/04_semantic_chunker.py
+++ code/C2/04_semantic_chunker.py
@@ -1,5 +1,5 @@
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
 
 embeddings = HuggingFaceEmbeddings(
@@ -8,19 +8,23 @@ embeddings = HuggingFaceEmbeddings(
     encode_kwargs={'normalize_embeddings': True}
 )
 
-# 初始化 SemanticChunker
-text_splitter = SemanticChunker(
-    embeddings,
-    breakpoint_threshold_type="percentile" # 也可以是 "standard_deviation", "interquartile", "gradient"
-)
+# 初始化 SemanticChunker，语义相似度进行文本分割的
+def source():
+    text_splitter = SemanticChunker(
+        embeddings,
+        breakpoint_threshold_type="percentile",  # 也可以是 "standard_deviation", "interquartile", "gradient"
+        breakpoint_threshold_amount=50 # 50%的语义相似度
+    )
 
-loader = TextLoader("../../data/C2/txt/蜂医.txt", encoding="utf-8")
-documents = loader.load()
+    loader = TextLoader("../../data/C2/txt/蜂医.txt", encoding="utf-8")
+    documents = loader.load()
 
-docs = text_splitter.split_documents(documents)
+    docs = text_splitter.split_documents(documents)
 
-print(f"文本被切分为 {len(docs)} 个块。\n")
-print("--- 前2个块内容示例 ---")
-for i, chunk in enumerate(docs[:2]):
-    print("=" * 60)
-    print(f'块 {i+1} (长度: {len(chunk.page_content)}):\n"{chunk.page_content}"')
+    print(f"文本被切分为 {len(docs)} 个块。\n")
+    print("--- 前2个块内容示例 ---")
+    for i, chunk in enumerate(docs[:2]):
+        print("=" * 60)
+        print(f'块 {i + 1} (长度: {len(chunk.page_content)}):\n"{chunk.page_content}"')
+if __name__ == '__main__':
+    source()
